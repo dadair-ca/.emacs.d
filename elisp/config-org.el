25ff8052af1ad6033a272e97b1a37624af4f6dc7
David Adair <david@cohesic.com>
2018-07-19 15:38:33 -0600

    List stuck projects in agenda and use note-specific file for note capture

--- elisp/config-org.el
+++ elisp/config-org.el
@@ -1,4 +1,4 @@
-(setq org-agenda-files (quote ("~/Dropbox/org/todo.org"
+(setq org-agenda-files (quote ("~/Dropbox/org/gtd.org"
                                "~/Dropbox/org/cohesic.org"
                                "~/Dropbox/org/refile.org"
                                "~/Dropbox/org/refile-beorg.org")))
@@ -19,7 +19,7 @@
 (setq org-treat-S-cursor-todo-selection-as-state-change nil)
 
 (setq org-directory "~/Dropbox/org")
-(setq org-default-notes-file "~/Dropbox/org/refile.org")
+(setq org-default-notes-file "~/Dropbox/org/notes.org")
 
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-c a") 'org-agenda)
@@ -27,7 +27,7 @@
 (setq org-capture-templates
       (quote (("t" "todo" entry (file "~/Dropbox/org/refile.org")
                "* TODO %?\n%U\n%a\n")
-              ("n" "note" entry (file "~/Dropbox/org/refile.org")
+              ("n" "note" entry (file "~/Dropbox/org/notes.org")
                "* %? :NOTE:\n%U\n%a\n")
               ("h" "Habit" entry (file "~/Dropbox/org/refile.org")
                "* TODO %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: TODO\n:END:\n")
@@ -43,6 +43,13 @@
 
 (setq org-agenda-skip-scheduled-if-done t)
 (setq org-agenda-skip-deadline-if-done t)
+(setq org-startup-indented t)
+(setq org-agenda-start-on-weekday nil)
+(setq org-agenda-span 7)
+(setq org-reverse-note-order t)
+(setq org-deadline-warning-days 14)
+(setq org-agenda-show-all-dates t)
+(setq org-fast-tag-selection-single-key (quote expert))
 
 (org-babel-do-load-languages
  'org-babel-load-languages
@@ -62,6 +69,7 @@
                 (tags "REFILE"
                       ((org-agenda-overriding-header "Tasks to Refile")
                        (org-tags-match-list-sublevels nil)))
+                (org-agenda-list-stuck-projects)
                 (tags-todo "-CANCELLED/!NEXT"
                            ((org-agenda-overriding-header "Next Tasks")
                             (org-tags-match-list-sublevels t))))))))
