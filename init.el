42a9e470673d20262eb9177e0221c9254b0ca3cc
David Adair <david@cohesic.com>
2018-10-10 19:45:55 -0600

    Remove mu4e config, simply yasnippets config

--- init.el
+++ init.el
@@ -288,7 +288,8 @@
 (use-package yasnippet
   :ensure t
   :diminish yas-minor-mode
-  :init (load-library "config-yasnippet"))
+  :config
+  (yas-global-mode 1))
 
 (use-package yasnippet-snippets
   :ensure t)
