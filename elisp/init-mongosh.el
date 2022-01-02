90e2158ace4e756a604bfe4fbc8358f502c6ee82
David Adair <adair.david@gmail.com>
2022-01-01 17:43:45 -0700

    Extract `mongosh' package from configuration

--- elisp/init-mongosh.el
+++ elisp/init-mongosh.el
@@ -1,4 +1,4 @@
-;;; init-mongosh.el --- Configuration for mongosh.	-*- lexical-binding: t -*-
+;;; init-mongosh.el --- `mongosh' configuration.	-*- lexical-binding: t -*-
 
 ;; Copyright (C) 2021 David Adair
 
@@ -25,69 +25,15 @@
 
 ;;; Commentary:
 ;;
-;; Configuration for mongosh.
+;; `mongosh' configuration.
 ;;
 
 ;;; Code:
 
-(require 'comint)
-
-(defconst mongosh-keywords
-  '("use" "show" "exit" "Mongo" "connect" "it" "version" "load"))
-
-(defconst mongosh-font-lock-keywords
-  (list
-   `(,(concat "\\_<" (regexp-opt mongosh-keywords) "\\_>") . font-lock-keyword-face))
-  "Additional expressions to highlint in `mongosh-mode'.")
-
-(defvar mongosh-file-path "/usr/bin/mongosh"
-  "File path for the `mongosh' executable.")
-
-(defvar mongosh-mode-map
-  (let ((map (nconc (make-sparse-keymap) comint-mode-map)))
-    (define-key map "\t" 'completion-at-point)
-    map)
-  "Basic mode map for `mongosh'.")
-
-(defvar mongosh-prompt-regexp "^\\(> \\)+"
-  "Regexp matching the expected `mongosh' REPL prompt.")
-
-(defun mongosh (SECRET)
-  "Run an inferior instance of `mongosh' against the connection url contained in `pass' SECRET."
-  (interactive "sSecret: ")
-  (let* ((mongosh-program mongosh-file-path)
-         (buffer (comint-check-proc "Mongosh")))
-    (pop-to-buffer-same-window
-     (if (or buffer (not (derived-mode-p 'mongosh-mode))
-             (comint-check-proc (current-buffer)))
-         (get-buffer-create (or buffer "*Mongosh*"))
-       (current-buffer)))
-    (unless buffer
-      (apply 'make-comint-in-buffer
-             "Mongosh"
-             buffer
-             mongosh-program
-             nil
-             (list (car (split-string (shell-command-to-string (concat "pass" " " SECRET)) "\n"))))
-      (mongosh-mode))))
-
-(defun mongosh--initialize ()
-  "Helper function to initialize `mongosh'."
-  (setq comint-process-echoes t)
-  (setq comint-use-prompt-regexp t))
-
-(define-derived-mode mongosh-mode comint-mode "Mongosh"
-  "Major mode for `mongosh'.
-
-\\<mongosh-mode-map>"
-  nil "Mongosh"
-  (setq comint-prompt-regexp mongosh-prompt-regexp)
-  (setq comint-prompt-read-only t)
-  (set (make-local-variable 'paragraph-separate) "\\'")
-  (set (make-local-variable 'font-lock-defaults) '(mongosh-font-lock-keywords t))
-  (set (make-local-variable 'paragraph-start) mongosh-prompt-regexp))
-
-(add-hook 'mongosh-mode-hook 'mongosh--initialize)
+(use-package mongosh
+  :ensure nil
+  :custom
+  (mongosh-secret-command "pass"))
 
 (provide 'init-mongosh)
 
