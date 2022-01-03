9af31b81fd9ca10989a8a256473e2c36e876594b
David Adair <adair.david@gmail.com>
2022-01-03 13:52:24 -0700

    Fix org diary agenda display

--- elisp/org-settings.el
+++ elisp/org-settings.el
@@ -3,13 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-habit-show-all-today t)
  '(org-hide-emphasis-markers t)
  '(org-hide-leading-stars t)
  '(org-M-RET-may-split-line '((headline . nil) (default . t)))
  '(org-agenda-deadline-leaders '("!D!: " "D%02d: " "-D%02d: "))
  '(org-agenda-scheduled-leaders '("" "S%d: "))
  '(org-agenda-fontify-priorities t)
- '(org-agenda-include-diary t)
  '(org-agenda-prefix-format
    '((agenda . "  %-11c%?-12t% s")
      (timeline . "  % s")
