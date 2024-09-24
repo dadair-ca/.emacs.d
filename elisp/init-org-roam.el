80638f7b3dd0c92a9e7c63faab27ef663bf25b0b
David Adair <david.adair@neofinancial.com>
2024-09-24 09:18:35 -0600

    Updated org-roam keybindings

--- elisp/init-org-roam.el
+++ elisp/init-org-roam.el
@@ -32,26 +32,22 @@
 
 (use-package org-roam
   :ensure t
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert)
-         ;; Dailies
-         ("C-c n c" . org-roam-capture)
-         :map org-roam-dailies-map
-         ("c" . org-roam-dailies-capture-today)
-         ("t" . (lambda () (interactive) (org-roam-dailies-goto-today "d")))
-         ("y" . (lambda () (interactive) (org-roam-dailies-goto-yesterday 1 "d")))
-         ("m" . (lambda () (interactive) (org-roam-dailies-goto-tomorrow 1 "d")))
-         ("d" . (lambda () (interactive) (org-roam-dailies-goto-date t "d"))))
-  :bind-keymap
-  ("C-c n d" . org-roam-dailies-map)
   :config
   (setq org-roam-directory (file-truename "~/org/roam"))
   (org-roam-db-autosync-mode))
 
-(use-package org-roam-ui
-  :ensure t
-  :bind (("C-c n u" . org-roam-ui-open)))
+(use-package org-roam-ui :ensure t)
+
+(global-set-key (kbd "C-c n l") 'org-roam-buffer-toggle)
+(global-set-key (kbd "C-c n f") 'org-roam-node-find)
+(global-set-key (kbd "C-c n l") 'org-roam-node-insert)
+(global-set-key (kbd "C-c n l") 'org-roam-capture)
+(global-set-key (kbd "C-c n u") 'org-roam-ui-open)
+
+(global-set-key (kbd "C-c n d t") (lambda () (interactive) (org-roam-dailies-capture-today nil "d")))
+(global-set-key (kbd "C-c n d y") (lambda () (interactive) (org-roam-dailies-capture-yesterday 1 "d")))
+(global-set-key (kbd "C-c n d m") (lambda () (interactive) (org-roam-dailies-capture-tomorrow 1 "d")))
+(global-set-key (kbd "C-c n d d") (lambda () (interactive) (org-roam-dailies-capture-date t "d")))
 
 (provide 'init-org-roam)
 
