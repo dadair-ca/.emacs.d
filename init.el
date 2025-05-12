a7d40e828be972e9fbb608cb8cf946524117504e
David Adair <adair.david@gmail.com>
2025-05-12 15:58:56 -0600

    humble [re]beginnings

--- init.el
+++ init.el
@@ -32,107 +32,12 @@
 
 ;;; Code:
 
-(when (version< emacs-version "25.1")
-  (error "This requires Emacs 25.1 and above!"))
-
-;; Speed up startup
-(defvar file-name-handler-alist-old file-name-handler-alist)
-(setq package-enable-at-startup nil
-      file-name-handler-alist nil
-      message-log-max 16384
-      gc-cons-threshold 402653184
-      gc-cons-percentage 0.6
-      auto-window-vscroll nil)
-
-(add-hook 'after-init-hook
-          `(lambda ()
-             (setq file-name-handler-alist file-name-handler-alist-old
-                   gc-cons-threshold 800000
-                   gc-cons-percentage 0.1)
-             (if (boundp 'after-focus-change-function)
-                 (add-function :after after-focus-change-function
-                               (lambda ()
-                                 (unless (frame-focus-state)
-                                   (garbage-collect))))
-               (add-hook 'focus-out-hook 'garbage-collect))))
-
-;; Load path
 (defun update-load-path (&rest _)
   "Update LOAD-PATH."
-  (push (expand-file-name "site-lisp" user-emacs-directory) load-path)
   (push (expand-file-name "elisp" user-emacs-directory) load-path))
 
-(defun add-subdirs-to-load-path (&rest _)
-  "Add subdirectories to the LOAD-PATH."
-  (let ((default-directory (expand-file-name "site-lisp" user-emacs-directory)))
-    (normal-top-level-add-subdirs-to-load-path)))
-
 (advice-add #'package-initialize :after #'update-load-path)
-(advice-add #'package-initialize :after #'add-subdirs-to-load-path)
 
 (update-load-path)
-
-(setq comp-async-report-warnings-errors nil)
-
-(require 'init-package)
-(require 'init-custom)
-
-(require 'init-alert)
-(require 'init-basic)
-
-(use-package company :ensure t)
-
-(require 'init-ace)
-;;(require 'init-dashboard)
-(require 'init-diary)
-;;(require 'init-denote)
-(require 'init-dired)
-(require 'init-ediff)
-(require 'init-editing)
-(require 'init-funs)
-(require 'init-org)
-(require 'init-org-roam)
-(require 'init-path)
-(require 'init-prog)
-(require 'init-project)
-(require 'init-search)
-(require 'init-ui)
-(require 'init-vcs)
-
-(use-package vertico
-  :init
-  (vertico-mode))
-
-(use-package savehist
-  :init
-  (savehist-mode))
-
-(use-package emacs
-  :init
-  ;; Add prompt indicator to `completing-read-multiple'.
-  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
-  (defun crm-indicator (args)
-    (cons (format "[CRM%s] %s"
-                  (replace-regexp-in-string
-                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
-                   crm-separator)
-                  (car args))
-          (cdr args)))
-  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
-
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
-        '(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
-  ;; Vertico commands are hidden in normal buffers.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
-
-  ;; Enable recursive minibuffers
-  (setq enable-recursive-minibuffers t))
-
-(setq completion-styles '(basic substring partial-completion emacs22))
 
 ;;; init.el ends here
