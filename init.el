9d05b0e9f5fb7b74df94c6a89a83acd976219be6
David Adair <adair.david@gmail.com>
2025-06-05 12:05:47 -0600

    homw=>denote;+vertico,marginalia,orderless,consult,magit

--- init.el
+++ init.el
@@ -46,6 +46,8 @@
 
 (update-load-path)
 
+(setq backup-directory-alist `(("." . "~/.saves")))
+
 (use-package gptel
   :bind (("C-c g RET" . gptel-send)
 	 ("C-c g /" . gptel))
@@ -61,28 +63,125 @@
      (writing . "You are a large language model and a writing assistant. Respond concisely.")
      )))
 
-(use-package howm
-  :ensure t)
-
-(use-package which-key
+(use-package org
   :ensure t
+  :defer t
+  :bind (("C-c a" . org-agenda)
+         ("C-c c" . org-capture)
+	 ("C-c l" . org-store-link))
+  :init
+  (setq org-directory "~/org")
+  (setq org-agenda-files '("~/org"))
+  (setq org-default-notes-file "~/org/inbox-MPro0147.org")
+  (setq
+   org-refile-targets
+   '((org-agenda-files :maxlevel . 2)
+     (nil :maxlevel . 2)))
+  (setq
+   org-capture-templates
+   '(("t" "Task" entry
+      (file "~/org/inbox-MPro0147.org")
+      "* TODO %?\n%U\n%a"))))
+   
+
+
+;; (use-package howm
+;;   :ensure t
+;;   :config
+;;   (setq howm-view-use-grep t)
+;;   (setq howm-directory "~/howm/")
+;;   (setq howm-menu-file "0000-00-00-000000.txt")
+;;   (setq howm-keyword-file (expand-file-name ".howm-keys" howm-directory))
+;;   (setq howm-history-file (expand-file-name ".howm-history" howm-directory)))
+
+(use-package uniline :ensure t)
+
+(use-package vertico
+  :ensure t
+  :custom
+  (vertico-resize t)
+  (vertico-cycle t)
+  :init
+  (vertico-mode))
+
+(use-package savehist
+  :init
+  (savehist-mode))
+
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package marginalia
+  :ensure t
+  :bind (:map minibuffer-local-map
+	      ("M-A" . marginalia-cycle))
+  :init
+  (marginalia-mode))
+
+(use-package consult
+  :ensure t
+  :bind ("C-x b" . consult-buffer))
+
+
+(use-package denote
+  :ensure t
+  :hook (dired-mode . denote-dired-mode)
+  :bind
+  (("C-c n o" . denote-open-or-create)
+   ("C-c n l" . denote-link-or-create)
+   ("C-c n b" . denote-backlinks)
+   ("C-c n d" . denote-dired)
+   ("C-c n g" . denote-grep))
   :config
-  (which-key-mode))
+  (setq denote-directory (expand-file-name "~/denote"))
+  (setq denote-date-prompt-use-org-read-date t)
+  (setq denote-infer-keywords nil)
+  (setq
+   denote-known-keywords
+   '("emacs"
+     "journal"
+     "nl" ;; Neo lending
+     "nc" ;; Neo card
+     ))
+  (denote-rename-buffer-mode 1))
+
+(use-package denote-journal
+  :ensure t
+  :hook (calendar-mode . denote-journal-calendar-mode)
+  :bind
+  (("C-c n j o" . denote-journal-new-or-existing-entry)
+   ("C-c n j l" . denote-journal-link-or-create-entry))
+  :config
+  (setq denote-journal-directory (expand-file-name "journal" denote-directory))
+  (setq denote-journal-keyword "journal")
+  (setq denote-journal-title-format 'day-date-month-year))
+
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status)))l
 
 (pixel-scroll-precision-mode)
+
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+
+;;; init.el ends here
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 140 :width normal :foundry "nil" :family "Menlo")))))
-
-;;; init.el ends here
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 140 :width normal :foundry "nil" :family "Menlo"))))
+ '(emoji ((t (:height 110 :family "Apple Color Emoji")))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key howm)))
+ '(package-selected-packages
+   '(magit consult marginalia orderless vertico orgalist uniline denote-journal denote which-key howm)))
