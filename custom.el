7e02b11dbdd8f6967b038d752d159b3373f4fbbc
David Adair <david.adair@neofinancial.com>
2026-06-17 17:18:50 -0600

    Revert "Wed 17 Jun 2026 12:48:33 MDT"
    This reverts commit 09edca7e51681e6a1270f6a3ecea87126bcf0fd5.

--- custom.el
+++ custom.el
@@ -3,8 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(consult consult-vulpea marginalia orderless vertico vulpea-journal)))
+ '(package-selected-packages '(beancount boxquote))
+ '(package-vc-selected-packages
+   '((beancount :url "https://github.com/beancount/beancount-mode"))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
