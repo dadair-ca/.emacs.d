b28914621ac9ba4c60045a2a0228c0620e903ac3
David Adair <david.adair@neofinancial.com>
2026-06-17 17:18:46 -0600

    Revert "Wed 17 Jun 2026 17:17:42 MDT"
    This reverts commit f1ba6787ae5131979dd13d180e1bf5a9b36d2079.

--- custom.el
+++ custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(consult-vulpea exec-path-from-shell marginalia orderless vertico
-		    vulpea-journal)))
+   '(consult consult-vulpea marginalia orderless vertico vulpea-journal)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
